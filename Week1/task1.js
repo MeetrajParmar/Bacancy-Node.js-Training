5527802bed1ecb63dfb1d423e6903a3af48fc1b0
Meerchi2754 <meetrajparmar556@gmail.com>
2026-01-13 17:45:35 +0530

    Adding git ignore

---
+++ Week1/task1.js
@@ -0,0 +1,11 @@
+//ASSIGNMENT 1: Number Check
+// const prompt = require("prompt-sync")();
+let num = prompt("Enter the number:");
+
+if (num > 0) {
+  console.log(`The Number ${num} is Positive.`);
+} else if (num === 0) {
+  console.log(`The Number ${num} is Zero.`);
+} else {
+  console.log(`The Number ${num} is Negative.`);
+}
