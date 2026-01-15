cdfa23e9b6766b1ca95cf32d75138fb215085bc6
Meerchi2754 <meetrajparmar556@gmail.com>
2026-01-15 12:25:56 +0530

    Week1 Task Complete!!

--- Week1/task5.js
+++ Week1/task5.js
@@ -1,7 +1,19 @@
 const prompt = require("prompt-sync")();
-let applyCoupon = (amount, couponCode) => {};
+let applyCoupon = (amount, couponCode) => {
+  couponCode = couponCode.toLowerCase();
+  if (couponCode == "none") {
+    return amount;
+  } else {
+    let discount = parseInt(couponCode.substring(4, couponCode.length));
+    if (discount > amount) return "Coupon Code not applicable";
+    return amount - discount;
+  }
+};
 
-let amount = prompt("Enter the Amount:");
-let coupon = prompt("Enter the Coupon Code:");
+// let amount = prompt("Enter the Amount:");
+// let coupon = prompt("Enter the Coupon Code:");
 
-console.log(applyCoupon(amount, coupon));
+console.log("Total:", applyCoupon(100, "SAVE100"));
+console.log("Total:", applyCoupon(300, "SAVE600"));
+console.log("Total:", applyCoupon(1000, "SAVE300"));
+console.log("Total:", applyCoupon(700, "SAVE600"));
